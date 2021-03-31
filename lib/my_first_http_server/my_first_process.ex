779d6644ae12e0bae0acfb39487cdb0c437ded97
JuliaMathias <juliamss@gmail.com>
2021-03-31 15:02:57 -0300

    Create loop function

--- lib/my_first_http_server/my_first_process.ex
+++ lib/my_first_http_server/my_first_process.ex
@@ -7,6 +7,21 @@ defmodule MyFirstHTTPServer.MyProcess do
 
   def start do
     IO.puts("Starting my first process")
+    loop()
   end
 
+  def loop do
+    receive do
+      :hello ->
+        IO.puts("Received :hello")
+        loop()
+
+      :exit ->
+        IO.puts("Exiting process")
+
+      msg ->
+        IO.puts("Received unknown message #{inspect(msg)}")
+        loop()
+    end
+  end
 end
