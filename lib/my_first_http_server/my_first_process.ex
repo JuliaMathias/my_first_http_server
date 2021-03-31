1a3ac0f4785da1c606cfc1d162c6b2972b9e486a
JuliaMathias <juliamss@gmail.com>
2021-03-31 15:25:24 -0300

    Refactor loop function to include state

--- lib/my_first_http_server/my_first_process.ex
+++ lib/my_first_http_server/my_first_process.ex
@@ -7,21 +7,26 @@ defmodule MyFirstHTTPServer.MyProcess do
 
   def start do
     IO.puts("Starting my first process")
-    loop()
+    loop(%{})
   end
 
-  def loop do
+  def loop(state) do
     receive do
-      {from, :hello} when is_pid(from) ->
-        send(from, :world)
-        loop()
+
+      {from, :put, key, value} ->
+        IO.puts("Adding key=#{inspect(key)} value=#{inspect(value)} ")
+        send(from, :key_value_added)
+        loop(Map.put(state, key, value))
 
       :exit ->
         IO.puts("Exiting process")
 
       msg ->
-        IO.puts("Received unknown message #{inspect(msg)}")
-        loop()
+        IO.puts("""
+        Received unknown message #{inspect(msg)}
+        Current state: #{inspect(state)}
+        """)
+        loop(state)
     end
   end
 end
